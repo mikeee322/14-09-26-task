7b3d1e035180d95f4c0dff87af0b5ea9f1b12112
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-09-18 00:37:09 +0500

    done task with timer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <thread>
 #include <iostream>
+#include <chrono>
+
+namespace mtt{
+  class Clicker {
+  public:
+    Clicker():
+      start_(std::chrono::high_resolution_clock::now())
+    {}
+    double millisec() const {
+      using std::chrono::high_resolution_clock;
+      using std::chrono::duration_cast;
+      using std::chrono::milliseconds;
+      auto t = high_resolution_clock::now();
+      return static_cast< double > (std::chrono::duration_cast< milliseconds >(t - start_).count());
+    }
+  private:
+    std::chrono::time_point< std::chrono::high_resolution_clock > start_;
+  };
+}
 
 using data_t = std::vector< unsigned long long >;
 using value_t = data_t::value_type;
@@ -13,17 +32,21 @@ void sumFunc(const data_t& values, size_t begin, size_t end, value_t &sum)
 }
 
 int main(int argc, char **argv){
-  data_t values(10, 1);
-  value_t sum{0};
+  if (argc < 2)
+  {
+    std::cerr << "err\n";
+    return 1;
+  }
+  constexpr size_t size{1'000};
+  data_t values(size, 1);
   size_t thread_count = 0;
   thread_count = std::stoull(argv[1]);
   size_t partSize = values.size() / thread_count;
 
-
-
   std::vector< std::thread > thrs;
-  std::vector<value_t> sums(thread_count, 0);
+  std::vector< value_t > sums(thread_count, 0);
 
+  mtt::Clicker cl;
   for (size_t i = 0; i < thread_count; ++i) {
     size_t begin = partSize * i;
     size_t end = begin + partSize;
@@ -40,10 +63,15 @@ int main(int argc, char **argv){
     thrs[i].join();
   }
 
+  double totalTime = cl.millisec();
+
   value_t total{0};
   for (size_t i = 0; i < thread_count; ++i) {
     total += sums[i];
   }
+
+  std::cout << "Sum: " << total << '\n';
+  std::cout << "Time: " << totalTime << " ms\n";
 
 
 
