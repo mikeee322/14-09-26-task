64e9b2866ef2893934367941c3b796a8b459eede
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-09-17 23:58:37 +0500

    done task without cliker time test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <thread>
+#include <iostream>
 
 using data_t = std::vector< unsigned long long >;
 using value_t = data_t::value_type;
@@ -12,15 +13,39 @@ void sumFunc(const data_t& values, size_t begin, size_t end, value_t &sum)
 }
 
 int main(int argc, char **argv){
-  constexpr size_t size{1'000'000'000};
-  data_t values(size, 1);
-  size_t theard_count = 0;
-  theard_count = std::stoull(argv[1]);
+  data_t values(10, 1);
+  value_t sum{0};
+  size_t thread_count = 0;
+  thread_count = std::stoull(argv[1]);
+  size_t partSize = values.size() / thread_count;
+
+
+
   std::vector< std::thread > thrs;
-  value_t sum = 0;
+  std::vector<value_t> sums(thread_count, 0);
 
-  std::thread thr(sumFunc, std::ref(values), 0, values.size(), std::ref(sum));
+  for (size_t i = 0; i < thread_count; ++i) {
+    size_t begin = partSize * i;
+    size_t end = begin + partSize;
+    if (i == thread_count - 1) {
+      end = begin + partSize + values.size() % thread_count;
+    }
 
-  thr.join();
+    std::thread thr(sumFunc, std::ref(values), begin, end, std::ref(sums[i]));
+
+    thrs.push_back(std::move(thr));
+  }
+
+  for (size_t i = 0; i < thread_count; ++i) {
+    thrs[i].join();
+  }
+
+  value_t total{0};
+  for (size_t i = 0; i < thread_count; ++i) {
+    total += sums[i];
+  }
+
+
 
 }
+
